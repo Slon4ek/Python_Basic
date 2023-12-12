394d4c6ec12d4d48d82b948b8abda4e18ec2e858
Slon4ek <Slon4ek2017@yandex.ru>
2023-12-12 14:38:54 +0300

    del test

--- Module14/01_os_info/main.py
+++ Module14/01_os_info/main.py
@@ -6,6 +6,7 @@ info = 'OS info is \n{}\n\nPython version is {} {}'.format(
     sys.version,
     platform.architecture(),
 )
+print('Системная информация:')
 print(info)
 
 with open('os_info.txt', 'w', encoding='utf8') as file:
