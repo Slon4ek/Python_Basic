e4e1f169bfa389923fb1e8294cb69f9724583b22
Slon4ek <Slon4ek2017@yandex.ru>
2023-12-15 14:15:00 +0300

    Программа для формирования списка нечетных чисел от 1 до N

--- Module15/01_list_gen/main.py
+++ Module15/01_list_gen/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+user_num = int(input('Введите число: '))
+num_list = []
+
+for i in range(1, user_num + 1, 2):
+    num_list.append(i)
+
+print(f'Список из нечётных чисел от одного до {user_num}: {num_list}')
