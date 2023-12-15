05f851a1c35ae89000bcca9d7110dc09dca94360
Slon4ek <Slon4ek2017@yandex.ru>
2023-12-15 14:38:07 +0300

    Task 3 complete

--- Module15/03_videocards/main.py
+++ Module15/03_videocards/main.py
@@ -1,3 +1,17 @@
-# TODO здесь писать код
+vcard_list = [3070, 2060, 3090, 3070, 3090]
+new_vcard_list = []
+max_ID = max(vcard_list)
 
+print(f'Количество видеокарт: {len(vcard_list)}')
 
+for i, ID in enumerate(vcard_list):
+    print(f'Видеокарта {i + 1}: {ID}')
+
+for card in vcard_list:
+    if card == max_ID:
+        continue
+    else:
+        new_vcard_list.append(card)
+
+print(f'Старый список видеокарт: {vcard_list}')
+print(f'Новый список видеокарт: {new_vcard_list}')
