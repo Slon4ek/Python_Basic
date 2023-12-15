1e7dde85cf110ae5aa0dde7947254e88b6098e60
Slon4ek <Slon4ek2017@yandex.ru>
2023-12-15 14:22:27 +0300

    Программа вывода элементов списка с четными индексами

--- Module15/02_tournament/main.py
+++ Module15/02_tournament/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+name_list = ['Артемий', 'Борис', 'Влад', 'Гоша', 'Дима', 'Евгений', 'Женя', 'Захар']
+first_day_list = []
+
+for i, name in enumerate(name_list):
+    if i % 2 == 0:
+        first_day_list.append(name_list[i])
+
+print(first_day_list)
