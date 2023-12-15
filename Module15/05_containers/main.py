2748d76316cf1e6dcf041cd04e6ccb1b556064cb
Slon4ek <Slon4ek2017@yandex.ru>
2023-12-15 16:12:23 +0300

    Task 5 complete

--- Module15/05_containers/main.py
+++ Module15/05_containers/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+container_count = int(input('Введите количество контейнеров: '))
+container_list = []
+
+for i in range(container_count):
+    print('Введите вес контейнера:', end=' ')
+    container = int(input())
+    while container > 200:
+        print('Вес контейнера не должен превышать 200кг')
+        container = int(input('Введите вес контейнера: '))
+    container_list.append(container)
+else:
+    list.sort(container_list, reverse=True)
+
+print(container_list)
+new_container = int(input('Введите вес нового контейнера: '))
+new_container_point = 1
+
+for i in range(len(container_list)):
+    if new_container <= container_list[i]:
+        new_container_point += 1
+
+print(f'Номер, который получит новый контейнер: {new_container_point}')
