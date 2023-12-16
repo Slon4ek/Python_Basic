34491f5fe89f810911648a5d4a72aa65e8176405
Slon4ek <Slon4ek2017@yandex.ru>
2023-12-16 10:29:17 +0300

    Task 6 fixed

--- Module15/06_running_nums/main.py
+++ Module15/06_running_nums/main.py
@@ -1,22 +1,16 @@
 num_count = int(input('Кол-во чисел в списке: '))
 numbers_list = []
 new_numbers_list = []
-start_new_list = []
-end_new_list = []
 
 for _ in range(num_count):
     print('Введите число:', end=' ')
     numbers_list.append(int(input()))
 
 shift = int(input('Сдвиг: '))
+index = - shift
 
-for index, num in enumerate(numbers_list):
-    if index >= shift - 1:
-        start_new_list.append(num)
-    else:
-        end_new_list.append(num)
+for _ in range(num_count):
+    new_numbers_list.append(numbers_list[index])
+    index += 1
 
-new_numbers_list = start_new_list + end_new_list
-
-print(f'Изначальный список: {numbers_list}\n'
-      f'Сдвинутый список: {new_numbers_list}')
+print(new_numbers_list)
