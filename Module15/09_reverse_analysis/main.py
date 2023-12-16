ce06e912269932ccf39e3657c79d9b9dc3108028
Slon4ek <Slon4ek2017@yandex.ru>
2023-12-16 12:20:22 +0300

    Task 9 complete

--- Module15/09_reverse_analysis/main.py
+++ Module15/09_reverse_analysis/main.py
@@ -1,4 +1,16 @@
-# TODO здесь писать код
+def list_sort(user_list):
+    for index_1 in range(len(user_list)):
+        for index_2 in range(index_1 + 1, len(user_list)):
+            if user_list[index_1] <= user_list[index_2]:
+                user_list[index_1], user_list[index_2] = user_list[index_2], user_list[index_1]
 
-# Список чисел для работы (итоговый алгоритм проверьте на разных списках, придуманных самостоятельно):
+
 numbers_list = [7, 14, 3, 18, 21, 10, 9, 6]
+list_sort(numbers_list)
+
+for i in range(len(numbers_list) - 1, -1, -1):
+    num = numbers_list[i]
+    if num % 2 != 0:
+        numbers_list.remove(num)
+
+print(numbers_list)
