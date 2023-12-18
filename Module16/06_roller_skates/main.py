638c5a6905d39a8dbd7efee08b7a9b1c070f026e
Slon4ek <Slon4ek2017@yandex.ru>
2023-12-18 17:46:27 +0300

    Task 6 complete

--- Module16/06_roller_skates/main.py
+++ Module16/06_roller_skates/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+rolls_list = []
+humans_list = []
+roll_count = int(input('Количество роликов: '))
+
+for i in range(1, roll_count + 1):
+    print(f'Размер пары {i}: ', end='')
+    size = int(input())
+    rolls_list.append(size)
+
+humans_count = int(input('Количество людей: '))
+hum_in_rolls = 0
+
+for i in range(1, humans_count + 1):
+    print(f'Размер ноги человека {i}: ', end='')
+    size = int(input())
+    humans_list.append(size)
+    if size in rolls_list:
+        hum_in_rolls += 1
+        rolls_list.remove(size)
+
+print(f'Наибольшее количество людей, которые могут взять ролики: {hum_in_rolls}')
