3c620781220224c4c6116acfaaac52c9f9103da6
Slon4ek <Slon4ek2017@yandex.ru>
2023-12-18 21:58:50 +0300

    Task 7 complete

--- Module16/07_rhyme_cnt/main.py
+++ Module16/07_rhyme_cnt/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+num_of_per = int(input('Кол-во человек: '))
+persons_list = list(range(1, num_of_per + 1))
+out_per_num = int(input('Какое число в считалке? '))
+print(f'Значит, выбывает каждый {out_per_num}-й человек')
+i_per = 0
+out_per = 0
+
+while len(persons_list) > 1:
+    print(f'Текущий круг людей: {persons_list}')
+    start_num = i_per % len(persons_list)
+    print(f'Начало счёта с номера {persons_list[start_num]}')
+    i_per = (i_per + out_per_num - 1) % len(persons_list)
+    out_per = persons_list.pop(i_per)
+    print(f'Выбывает человек под номером {out_per}')
+
+print(f'\nОстался человек под номером: {persons_list[0]}')
