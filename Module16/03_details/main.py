4fc2f20c3d6f0bd52e997d3470075bbb6a29cc80
Slon4ek <Slon4ek2017@yandex.ru>
2023-12-18 13:23:47 +0300

    Task 3 complete

--- Module16/03_details/main.py
+++ Module16/03_details/main.py
@@ -2,4 +2,14 @@ shop = [['каретка', 1200], ['шатун', 1000], ['седло', 300],
         ['педаль', 100], ['седло', 1500], ['рама', 12000],
         ['обод', 2000], ['шатун', 200], ['седло', 2700]]
 
-# TODO здесь писать код
+total_goods = 0
+total_cost = 0
+user_input = input('Введите название детали: ')
+
+for prod, cost in shop:
+    if user_input == prod:
+        total_goods += 1
+        total_cost += cost
+
+print(f'Кол-во деталей: {total_goods}\n'
+      f'Общая стоимость: {total_cost}')
