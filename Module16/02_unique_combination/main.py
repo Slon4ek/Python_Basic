68a4a80e7036bf4f5b2ec38d6656443f9fb250d9
Slon4ek <Slon4ek2017@yandex.ru>
2023-12-18 12:41:56 +0300

    Task 2 complete

--- Module16/02_unique_combination/main.py
+++ Module16/02_unique_combination/main.py
@@ -1,8 +1,13 @@
-# TODO здесь писать код
+def merge_sorted_lists(first_list, second_list):
+    first_list.extend(second_list)
+    for i_fl in first_list:
+        if first_list.count(i_fl) > 1:
+            first_list.remove(i_fl)
+    return sorted(first_list)
 
 
 # Пример использования:
 list1 = [1, 3, 5, 7, 9]
-list2 = [2, 4, 5, 6, 8, 10]
+list2 = [2, 4, 6, 8, 10]
 merged = merge_sorted_lists(list1, list2)
 print(merged)
