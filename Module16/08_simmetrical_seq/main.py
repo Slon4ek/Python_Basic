2fc4d2105d29fc35febf5c1cdc861efe8a95489e
Slon4ek <Slon4ek2017@yandex.ru>
2023-12-19 15:58:47 +0300

    Task 8 List comprehensions

--- Module16/08_simmetrical_seq/main.py
+++ Module16/08_simmetrical_seq/main.py
@@ -1,18 +1,16 @@
 num_quantity = int(input('Введите количество чисел: '))
-numbers = []
-
-for i in range(num_quantity):
-    print(f'Число {i + 1}: ', end='')
-    user_num = int(input())
-    numbers.append(user_num)
-
+numbers = [int(input('Введите число: ')) for _ in range(num_quantity)]
 add_nums = []
-for i_num in range(0, len(numbers)):
-    if numbers[i_num:] == numbers[:i_num - 1: -1]:
-        add_nums = numbers[:i_num]
-        add_nums.reverse()
-        break
-    tmp_num_list = []
-print(f'Последовательность чисел: {numbers}\n'
-      f'Нужно приписать чисел: {len(add_nums)}\n'
-      f'Сами числа: {add_nums}')
+
+if numbers == numbers[::-1]:
+    print('Последовательность симметрична, ничего добавлять не нужно.')
+else:
+    for i_num in range(0, len(numbers)):
+        if numbers[i_num:] == numbers[:i_num - 1: -1]:
+            add_nums = numbers[:i_num]
+            add_nums.reverse()
+            break
+
+    print(f'Последовательность чисел: {numbers}\n'
+          f'Нужно приписать чисел: {len(add_nums)}\n'
+          f'Сами числа: {add_nums}')
