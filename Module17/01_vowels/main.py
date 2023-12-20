ee6a379ccfa071b2c0b2e48758cea67f592afebb
Slon4ek <Slon4ek2017@yandex.ru>
2023-12-20 10:35:23 +0300

    Task 1 complete

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+vowel_letters = ['а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е']
+user_input = input('Введите текст: ')
+vowel_in_text = [vow for vow in user_input if vow in vowel_letters]
+
+print(f'Список гласных букв: {vowel_in_text}\n'
+      f'Длина списка: {len(vowel_in_text)}')
