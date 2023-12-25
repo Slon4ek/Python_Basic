8fb05fc653cc22878f04e1f065456d0e65349a0b
Slon4ek <Slon4ek2017@yandex.ru>
2023-12-25 14:02:43 +0300

    Task 3 complete

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+import random
+
+
+def max_score(list_1, list_2, index):
+    return max(list_1[index], list_2[index])
+
+
+team_1 = [round(random.uniform(5, 10), 2) for _ in range(20)]
+team_2 = [round(random.uniform(5, 10), 2) for _ in range(20)]
+winners = [max_score(team_1, team_2, i_team) for i_team in range(20)]
+
+print(f'Первая команда: {team_1}\n'
+      f'Вторая команда: {team_2}\n'
+      f'Победители тура: {winners}')
