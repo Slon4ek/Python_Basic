82103ff44b8dc5533568ac44ce7605bba86749cb
Slon4ek <Slon4ek2017@yandex.ru>
2023-12-26 14:27:24 +0300

    Task 8 complete

--- Module17/08_caesar_cipher/main.py
+++ Module17/08_caesar_cipher/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+def encryption(txt, shift):
+    letter_list = [(alphabet[(alphabet.index(letter) + shift) % 33]
+                   if letter != ' '
+                   else ' ')
+                   for letter in txt]
+    encryption_txt = ''
+    for i_let in letter_list:
+        encryption_txt += i_let
+    return encryption_txt
+
+
+alphabet = [chr(i) for i in range(ord('а'), ord('я') + 1)]
+alphabet.insert(6, 'ё')
+
+user_txt = input('Введите сообщение: ')
+user_shift = int(input('Введите сдвиг: '))
+print(encryption(user_txt, user_shift))
