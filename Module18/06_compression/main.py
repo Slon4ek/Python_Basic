752fa84e5ee6ffe0bd4fffe30bbfc9d08686962d
Slon4ek <Slon4ek2017@yandex.ru>
2023-12-29 21:02:34 +0300

    Task 6 complete

--- Module18/06_compression/main.py
+++ Module18/06_compression/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+user_input = input('Введите строку: ')
+count = 1
+output_str = []
+for i in range(len(user_input)):
+    if user_input[i] == user_input[i + 1: i + 2]:
+        count += 1
+        continue
+    output_str.append(user_input[i] + str(count))
+    count = 1
+
+print(''.join(output_str))
