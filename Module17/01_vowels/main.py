fa3f1ca40e3a044aed3861cc26671e9261737d92
Slon4ek <Slon4ek2017@yandex.ru>
2023-12-29 11:22:39 +0300

    Task 8 complete

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -1,6 +1,4 @@
-vowel_letters = ['а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е']
-user_input = input('Введите текст: ')
-vowel_in_text = [vow for vow in user_input if vow in vowel_letters]
+vowel_in_text = [vow for vow in input('Введите текст: ') if vow.lower() in 'аеёиоуыэюя']
 
 print(f'Список гласных букв: {vowel_in_text}\n'
       f'Длина списка: {len(vowel_in_text)}')
