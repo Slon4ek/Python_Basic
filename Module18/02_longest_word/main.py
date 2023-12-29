f09b345660408db6585b5c1b09137db9e4377878
Slon4ek <Slon4ek2017@yandex.ru>
2023-12-29 13:57:11 +0300

    Task 2 complete

--- Module18/02_longest_word/main.py
+++ Module18/02_longest_word/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+input_str = input('Введите строку: ').split(' ')
+
+max_len = max(len(word) for word in input_str)
+max_word = ''
+
+for word in input_str:
+    if len(word) == max_len:
+        max_word = word
+        break
+
+if max_len == 1:
+    ending = ''
+elif 1 < max_len < 5:
+    ending = 'а'
+else:
+    ending = 'ов'
+
+print(f'Самое длинное слово: {max_word}\n'
+      f'Длина этого слова: {max_len} символ{ending}')
