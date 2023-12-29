f0ae908f547b28ea466494b6ec8eb6ec1748d272
Slon4ek <Slon4ek2017@yandex.ru>
2023-12-29 14:34:34 +0300

    Task 3 complete

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -1,9 +1,10 @@
 invalid_chr = ('@', '№', '$', '%', '^', '&', '*', '()')
+file_extension = ('.txt', '.docx')
 user_input = input('Название файла: ')
 
 if user_input.startswith(invalid_chr):
     print('Ошибка: название начинается недопустимым символом.')
-elif not user_input.endswith('.txt') and not user_input.endswith('.docx'):
+elif not user_input.endswith(file_extension):
     print('Ошибка: неверное расширение файла. Ожидалось .txt или .docx')
 else:
     print('Файл назван верно')
