c4d0414955362b7275bc683b87767df086ef6df8
Slon4ek <Slon4ek2017@yandex.ru>
2023-12-29 15:30:59 +0300

    Task 5 complete

--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+while True:
+    user_pass = input('Придумайте пароль: ')
+    if len([letter for letter in user_pass if letter.isupper()]) < 1\
+            or len([digit for digit in user_pass if digit.isdigit()]) < 3\
+            or len(user_pass) < 8:
+        print('Пароль ненадёжный. Попробуйте ещё раз.')
+    else:
+        print('Это надёжный пароль.')
+        break
