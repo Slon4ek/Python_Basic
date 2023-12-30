ff2f951d2af685cbeda3a65cf958687a042e4e11
Slon4ek <Slon4ek2017@yandex.ru>
2023-12-30 14:42:04 +0300

    Task 6 complete

--- Module18/06_compression/main.py
+++ Module18/06_compression/main.py
@@ -1,11 +1,11 @@
 user_input = input('Введите строку: ')
 count = 1
-output_str = []
+output_list = []
 for i in range(len(user_input)):
     if user_input[i] == user_input[i + 1: i + 2]:
         count += 1
         continue
-    output_str.append(user_input[i] + str(count))
+    output_list.append(user_input[i] + str(count))
     count = 1
-
-print(''.join(output_str))
+output_str = ''.join(output_list)
+print(f'Закодированная строка: {output_str}')
