476619965240a69730675105328e51b8d66de1bd
Slon4ek <Slon4ek2017@yandex.ru>
2023-12-30 16:48:32 +0300

    Task 8*

--- Module18/08_ticker/main.py
+++ Module18/08_ticker/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+input_str_1 = 'abcde'
+input_str_2 = 'cedab'
+shift = 0
+
+for i_1, sym_1 in enumerate(input_str_1):
+    for i_2, sym_2 in enumerate(input_str_2):
+        if sym_1 == sym_2:
+            shift = i_2 + 1
+            if input_str_1.startswith(''.join(input_str_2[i_2:])) and input_str_1.endswith(
+                    ''.join(input_str_2[:i_2])):
+                print(f'Первая строка получается из второй со сдвигом {shift}')
+                break
+            else:
+                print('Первую строку нельзя получить из второй с помощью циклического сдвига')
+                break
+    break
+
