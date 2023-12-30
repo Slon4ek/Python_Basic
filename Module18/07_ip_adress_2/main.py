38fb021a57e77e6bcd2fe939b71068be135334d0
Slon4ek <Slon4ek2017@yandex.ru>
2023-12-30 15:22:08 +0300

    Task 7 complete

--- Module18/07_ip_adress_2/main.py
+++ Module18/07_ip_adress_2/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+while True:
+    ip_address = input('Введите IP: ')
+    if ',' in ip_address:
+        print(f'Адрес — это четыре числа, разделённые точками')
+        continue
+
+    ip_address = ip_address.split('.')
+
+    for num in ip_address:
+        if not num.isnumeric():
+            print(f'{num} — это не целое число')
+            ip_address = 0
+            break
+        elif int(num) > 255:
+            print(f'{num} превышает 255')
+            ip_address = 0
+            break
+    if ip_address:
+        print('IP-адрес корректен')
+        break
+
