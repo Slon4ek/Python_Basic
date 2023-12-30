4eeac2586acb3d65eab4b3b0819ea29b2cfe656c
Slon4ek <Slon4ek2017@yandex.ru>
2023-12-30 17:45:32 +0300

    Task 8 complete

--- Module18/08_ticker/main.py
+++ Module18/08_ticker/main.py
@@ -1,8 +1,8 @@
 input_str_1 = 'abcde'
 input_str_2 = 'cdeab'
-start_sym = input_str_2.find(input_str_1[0])
+start_sym = input_str_1.find(input_str_2[0])
 
-if input_str_1.startswith(''.join(input_str_2[start_sym:])) and input_str_1.endswith(''.join(input_str_2[:start_sym])):
+if input_str_2.startswith(''.join(input_str_1[start_sym:])) and input_str_2.endswith(''.join(input_str_1[:start_sym])):
     print(f'Первая строка получается из второй со сдвигом {start_sym}')
 else:
     print('Первую строку нельзя получить из второй с помощью циклического сдвига')
