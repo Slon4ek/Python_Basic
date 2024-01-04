03c38147ff2df062a183a83759172fd52243b3e0
Slon4ek <Slon4ek2017@yandex.ru>
2024-01-04 14:53:01 +0300

    Task 1 complete

--- Module19/01_songs_2/main.py
+++ Module19/01_songs_2/main.py
@@ -9,5 +9,25 @@ violator_songs = {
     'Blue Dress': 4.29,
     'Clean': 5.83
 }
+num_dict = {
+    1: 'первой',
+    2: 'второй',
+    3: 'третьей',
+    4: 'четвертой',
+    5: 'пятой',
+    6: 'шестой',
+    7: 'седьмой',
+    8: 'восьмой',
+    9: 'девятой'
+}
+playing_time = 0
+num_songs = int(input('Введите количество песен: '))
 
-# TODO здесь писать код
+for num in range(1, num_songs + 1):
+    user_song = input(f'Введите название {num_dict[num]} песни: ')
+    while user_song not in violator_songs:
+        print('Такой песни нет :(')
+        user_song = input(f'Введите название {num_dict[num]} песни: ')
+    playing_time += violator_songs.get(user_song)
+
+print(f'Общее время звучания песен: {round(playing_time, 2)}мин')
