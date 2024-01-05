b40a09212ae6df595c2b3f976fb8da6dd6d6ca3c
Slon4ek <Slon4ek2017@yandex.ru>
2024-01-05 17:05:45 +0300

    add function 'no_set' add function 'with_set'

--- Module19/07_three_lists/main.py
+++ Module19/07_three_lists/main.py
@@ -1,19 +1,33 @@
+def no_set(a, b, c, task_num):
+    if task_num == 1:
+        print('Задача 1: ')
+        max_array = max(a, b, c)
+        print('Решение без множеств: ', end='')
+        for elem in max_array:
+            if elem in a and elem in b and elem in c:
+                print(elem, end=' ')
+    elif task_num == 2:
+        print('Задача 2: ')
+        print('Решение без множеств: ', end='')
+        for elem in a:
+            if elem not in b and elem not in c:
+                print(elem, end=' ')
+
+
+def with_set(a, b, c, task_num):
+    if task_num == 1:
+        sim_nums = set(a) & set(b) & set(c)
+        print(f'\nРешение с множествами: {sim_nums}')
+    elif task_num == 2:
+        diff_nums = set(a) - (set(b) | set(c))
+        print(f'\nРешение с множествами: {diff_nums}')
+
+
 array_1 = [1, 5, 10, 20, 40, 80, 100]
 array_2 = [6, 7, 20, 80, 100]
 array_3 = [3, 4, 15, 20, 30, 70, 80, 120]
 
-print('Задача 1: ')
-
-print('Решение без множеств: ', end='')
-for elem in array_3:
-    if elem in array_1 and elem in array_2:
-        print(elem, end=' ')
-
-print(f'\nРешение с множествами: {set(array_1) & set(array_2) & set(array_3)}')
-
-print('Решение без множеств: ', end='')
-for elem in array_1:
-    if elem not in array_2 and elem not in array_3:
-        print(elem, end=' ')
-
-print(f'\nРешение с множествами: {set(array_1) - (set(array_2) | set(array_3))}')
+no_set(array_1, array_2, array_3, 1)
+with_set(array_1, array_2, array_3, 1)
+no_set(array_1, array_2, array_3, 2)
+with_set(array_1, array_2, array_3, 2)
