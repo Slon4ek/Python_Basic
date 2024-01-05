ce127c14ecfe5205162aa9c7f19c262e751ef914
Slon4ek <Slon4ek2017@yandex.ru>
2024-01-05 20:08:49 +0300

    add function 'palindrome'

--- Module19/08_palindrome_again/main.py
+++ Module19/08_palindrome_again/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+def palindrome(string):
+    sym_dict = {
+        sym: string.count(sym)
+        for sym in string
+    }
+    odd_count = 0
+    for val in sym_dict.values():
+        if int(val) % 2 != 0:
+            odd_count += 1
+    if odd_count > 1:
+        print('Нельзя сделать палиндромом')
+    else:
+        print('Можно сделать палиндромом')
+
+
+user_str = input('Введите строку: ')
+palindrome(user_str)
