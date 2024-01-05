a0bf95cd9f670e32c7a6f494f6b94dea64f9786c
Slon4ek <Slon4ek2017@yandex.ru>
2024-01-05 16:40:38 +0300

    task 7 complete

--- Module19/07_three_lists/main.py
+++ Module19/07_three_lists/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+array_1 = [1, 5, 10, 20, 40, 80, 100]
+array_2 = [6, 7, 20, 80, 100]
+array_3 = [3, 4, 15, 20, 30, 70, 80, 120]
+
+print('Задача 1: ')
+
+print('Решение без множеств: ', end='')
+for elem in array_3:
+    if elem in array_1 and elem in array_2:
+        print(elem, end=' ')
+
+print(f'\nРешение с множествами: {set(array_1) & set(array_2) & set(array_3)}')
+
+print('Решение без множеств: ', end='')
+for elem in array_1:
+    if elem not in array_2 and elem not in array_3:
+        print(elem, end=' ')
+
+print(f'\nРешение с множествами: {set(array_1) - (set(array_2) | set(array_3))}')
