dd8ce80ca7e7958f0c6d40fe264d3de9a987219d
Slon4ek <Slon4ek2017@yandex.ru>
2024-01-08 17:11:47 +0300

    Task 2: add function 'is_prime' add function 'crypto'

--- Module20/02_universal_prog_2/main.py
+++ Module20/02_universal_prog_2/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+def is_prime(num):
+    divider = 0
+    for i in range(2, num // 2 + 1):
+        if num % i == 0:
+            divider += 1
+    if num <= 1 or divider != 0:
+        return False
+    else:
+        return True
+
+
+def crypto(u_list):
+    return list(sym for i_sym, sym in enumerate(u_list) if is_prime(i_sym))
+
+
+print(crypto('О Дивный Новый мир!'))
