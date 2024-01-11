1d73af831afad4a36d2a9b49c0d685b3e29c47b0
Slon4ek <Slon4ek2017@yandex.ru>
2024-01-11 13:19:46 +0300

    Task 6: add function add_contact add function search_contact

--- Module20/06_contacts_3/main.py
+++ Module20/06_contacts_3/main.py
@@ -1,1 +1,32 @@
-# TODO здесь писать код
+def add_contact(dictionary):
+    name, surname = input('Введите имя и фамилию нового контакта (через пробел): ').title().split()
+    if (name, surname) in dictionary.keys():
+        print(f'Такой человек уже есть в контактах.\n'
+              f'Текущий словарь контактов: {dictionary}')
+    else:
+        phone_num = input('Введите номер телефона: ')
+        dictionary[(name, surname)] = phone_num
+        print(f'Текущий словарь контактов: {dictionary}')
+
+
+def search_contact(dictionary):
+    surname = input('Введите фамилию для поиска: ').title()
+    for person in dictionary:
+        if surname in person:
+            print(f'{person[0]} {person[1]}: {dictionary[person]}')
+        else:
+            print(f'Человека с фамилией {surname} нет в списке контактов.')
+
+
+phone_dict = {}
+while True:
+    print('Введите номер действия\n'
+          '\t1. Добавить контакт\n'
+          '\t2. Найти человека')
+    answer = input()
+    if answer == '1':
+        add_contact(phone_dict)
+    elif answer == '2':
+        search_contact(phone_dict)
+    else:
+        break
