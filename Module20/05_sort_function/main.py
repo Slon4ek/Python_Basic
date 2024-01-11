d60c5ddf8913c0990dae96e20cbe5e069c61f736
Slon4ek <Slon4ek2017@yandex.ru>
2024-01-11 11:39:58 +0300

    add function tpl_sort

--- Module20/05_sort_function/main.py
+++ Module20/05_sort_function/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+def tpl_sort(tpl):
+    tpl_list = list(tpl)
+    float_flag = False
+    for _, num in enumerate(tpl_list):
+        if isinstance(num, float):
+            float_flag = True
+    if float_flag:
+        return tuple(tpl)
+    else:
+        for index_1 in range(len(tpl_list)):
+            for index_2 in range(index_1 + 1, len(tpl_list)):
+                if tpl_list[index_1] >= tpl_list[index_2]:
+                    tpl_list[index_1], tpl_list[index_2] = tpl_list[index_2], tpl_list[index_1]
+        return tuple(tpl_list)
