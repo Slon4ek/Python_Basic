be28c735befce5d0ce6996128b9b04d536070e31
Slon4ek <Slon4ek2017@yandex.ru>
2024-01-13 09:36:18 +0300

    Task 1: add function all_num

--- Module21/01_challenge_2/main.py
+++ Module21/01_challenge_2/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+def all_num(num):
+    if num == 1:
+        print(num)
+    else:
+        all_num(num - 1)
+        print(num)
+
+
+user_num = int(input('Введите число: '))
+all_num(user_num)
