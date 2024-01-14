5161f7b3021adc61dc081dbcef3041c0344d4bbd
Slon4ek <Slon4ek2017@yandex.ru>
2024-01-14 12:00:01 +0300

    Task 4: add function 'super_sum'

--- Module21/04_advanced_sum/main.py
+++ Module21/04_advanced_sum/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+def super_sum(*args):
+    result = 0
+    for elem in args:
+        if isinstance(elem, (int, float)):
+            result += elem
+        else:
+            for i, val in enumerate(elem):
+                result += super_sum(val)
+    return result
+
