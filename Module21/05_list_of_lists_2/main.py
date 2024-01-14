61ec5933b714c7b24ec8cdd5a99cdbdb24d6ee6c
Slon4ek <Slon4ek2017@yandex.ru>
2024-01-14 13:26:07 +0300

    Task 5: add function 'unfold_lists'

--- Module21/05_list_of_lists_2/main.py
+++ Module21/05_list_of_lists_2/main.py
@@ -1,4 +1,15 @@
 nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
              [[11, 12, 13], [14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+
+def unfold_lists(lst):
+    new_list = []
+    if isinstance(lst, (int, float, str)):
+        new_list.append(lst)
+    else:
+        for elem in lst:
+            new_list.extend(unfold_lists(elem))
+    return new_list
+
+
+print(unfold_lists(nice_list))
