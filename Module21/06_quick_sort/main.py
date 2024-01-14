6c7bb1412cdaa1700db418b2c9d95875c32b7b80
Slon4ek <Slon4ek2017@yandex.ru>
2024-01-14 18:38:32 +0300

    Task 6: add function 'quick_sort'

--- Module21/06_quick_sort/main.py
+++ Module21/06_quick_sort/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+def quick_sort(lst):
+    if not lst:
+        return []
+    support_num = lst[-1]
+    start_list = [num for num in lst if num < support_num]
+    mid_list = [num for num in lst if num == support_num]
+    end_list = [num for num in lst if num > support_num]
+    result = quick_sort(start_list) + mid_list + quick_sort(end_list)
+    return result
+
+
+user_list = [5, 8, 9, 4, 2, 9, 1, 8]
+print(quick_sort(user_list))
