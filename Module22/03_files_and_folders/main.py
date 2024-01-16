5b4f8974fcfaab10cc3c950280496770a89b4b65
Alexey <alexei@bk.ru>
2024-01-16 21:44:23 +0300

    example

--- Module22/03_files_and_folders/main.py
+++ Module22/03_files_and_folders/main.py
@@ -1,29 +1,23 @@
 import os
 
 
-def dir_info(path_to_dir):
-    dir_count = 0
-    file_count = 0
-    files_size = 0
-    for obj in os.listdir(path_to_dir):
-        path = os.path.abspath(os.path.join(path_to_dir, obj))
-        if os.path.isfile(path):
-            files_size += os.path.getsize(path)
-            file_count += 1
-        elif os.path.isdir(path):
-            dir_count += 1
-            dc, fc, fs = dir_info(path)
-            dir_count += dc
-            file_count += fc
-            files_size += fs
-    return dir_count, file_count, files_size
+def output(folder_size_bytes, subfolders_qty, files_qty):
+    print("Размер каталога (в Кб): {:.04f}".format(folder_size_bytes / 1024))
+    print("Количество подкаталогов:", subfolders_qty)
+    print("Количество файлов:", files_qty)
 
 
-work_dir = os.path.abspath(os.path.join(os.sep, 'SkillBox Python'))
-if os.path.exists(work_dir):
-    dir_quantity, file_quantity, dir_size = dir_info(work_dir)
-    print(f'Размер каталога (в Кбайтах): {dir_size / 1024}\n'
-          f'Количество подкаталогов: {dir_quantity}\n'
-          f'Количество файлов: {file_quantity}')
-else:
-    print('Такого каталога не существует!')
+folder_path = input('Пожалуйста, введите путь до директории: ')
+files_lst = []
+subdirs = []
+for root, dirs, files in os.walk(folder_path):
+    for name in files:
+        files_lst.append(os.path.join(root, name))
+    for name in dirs:
+        subdirs.append(os.path.join(root, name))
+
+folder_size_bytes = 0
+for file_name in files_lst:
+    folder_size_bytes += os.path.getsize(file_name)
+
+output(folder_size_bytes, len(subdirs), len(files_lst))
