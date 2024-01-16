ab296aa54a64177f1090a1a4a5e07e11f220998a
Slon4ek <Slon4ek2017@yandex.ru>
2024-01-16 16:37:53 +0300

    Homework Modul_22 Task 1: Complete

--- Module22/01_nums_sum_2/main.py
+++ Module22/01_nums_sum_2/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+read_file = open('numbers.txt', 'r')
+write_file = open('answer.txt', 'w')
+
+nums = read_file.read().split()
+summ = 0
+
+for num in nums:
+    summ += int(num)
+
+write_file.write(str(summ))
+
+read_file.close()
+write_file.close()
