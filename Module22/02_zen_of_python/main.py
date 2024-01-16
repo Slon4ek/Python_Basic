d685e45ae4526205d42bc802470a435d115bb0b5
Slon4ek <Slon4ek2017@yandex.ru>
2024-01-16 16:54:06 +0300

    Homework Modul_22 Task 2: add function 'invert_output'

--- Module22/02_zen_of_python/main.py
+++ Module22/02_zen_of_python/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+def invert_output(file_name):
+    file = open(file_name, 'r')
+    lines_list = [line for line in file]
+    for line in lines_list[::-1]:
+        print(line, end='')
+    file.close()
+
+
+invert_output('zen.txt')
