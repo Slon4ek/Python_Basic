22c120a89028e9d83488d7660389028ead53bd21
Slon4ek <Slon4ek2017@yandex.ru>
2024-01-17 15:24:56 +0300

    Homework Modul_22 Task 6: add function 'alphabet_generator' add function 'text_analyzer' task complete

--- Module22/06_war_and_peace/main.py
+++ Module22/06_war_and_peace/main.py
@@ -1,1 +1,38 @@
-# TODO здесь писать код
+import zipfile
+
+
+def alphabet_generator(start_sym, a_sym_quantity):
+    # Функция генерирует строку с алфавитом
+    alpha_str = ''
+    for i in range(1, a_sym_quantity + 1):
+        alpha_str += chr(start_sym) + chr(start_sym).upper()
+        start_sym += 1
+    return alpha_str
+
+
+def text_analyzer(file_name):
+    # Функция анализирует текст из файла и возвращает список букв и сколько раз они встречаются в тексте
+    file = open(file_name, 'r', encoding='utf-8')
+    text = file.read()
+    ru_alphabet = alphabet_generator(1072, 32)
+    en_alphabet = alphabet_generator(97, 26)
+    ru_sym_dict = {(sym, 'ru'): text.count(sym) for sym in ru_alphabet if sym in text}
+    en_sym_dict = {(sym, 'en'): text.count(sym) for sym in en_alphabet if sym in text}
+    all_sym_dict = {**ru_sym_dict, **en_sym_dict}
+    sort_sym = sorted(all_sym_dict.items(), key=lambda x: x[1], reverse=True)
+    file.close()
+    return sort_sym
+
+
+my_zip = zipfile.ZipFile('voyna-i-mir.zip', 'r')
+my_zip.extract('voyna-i-mir.txt')
+my_zip.close()
+
+analyze = text_analyzer('voyna-i-mir.txt')
+file_for_write = open('text_analyze.txt', 'w', encoding='utf-8')
+
+for elem in analyze:
+    string = ''.join('{} : {}'.format(elem[0], elem[1]))
+    file_for_write.write(string + '\n')
+
+file_for_write.close()
