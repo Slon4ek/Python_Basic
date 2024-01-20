71e765bab20726c09f9b38c460761d5ae2cc783c
Slon4ek <Slon4ek2017@yandex.ru>
2024-01-20 12:07:16 +0300

    Homework Modul_23 Task 1: complete

--- Module23/01_names_2/main.py
+++ Module23/01_names_2/main.py
@@ -2,18 +2,17 @@ sym_sum = 0
 line_count = 0
 
 try:
-    with open('people.txt', 'r', encoding='utf-8') as user_names, open('errors.log', 'w', encoding='utf-8') as errors:
-        try:
-            for i_line in user_names:
+    with open('people.txt', 'r', encoding='utf-8') as user_names, open('errors.log', 'a', encoding='utf-8') as errors:
+        for i_line in user_names:
+            try:
                 line = i_line.rstrip()
                 line_count += 1
                 if len(line) < 3:
                     raise ValueError
                 sym_sum += len(line)
-        except ValueError:
-            errors.write('Ошибка: менее трёх символов в строке {}\n'.format(line_count))
-            print('Ошибка: менее трёх символов в строке {}'.format(line_count))
-        finally:
-            print('Общее количество символов: {}'.format(sym_sum))
+            except ValueError:
+                errors.write('Ошибка: менее трёх символов в строке {}\n'.format(line_count))
+                print('Ошибка: менее трёх символов в строке {}'.format(line_count))
+        print('Общее количество символов: {}'.format(sym_sum))
 except FileNotFoundError as exc:
     print(exc, 'Файл не найден!')
