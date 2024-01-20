b74ac36602266bcfbb0b966282dfebbcd256299a
Slon4ek <Slon4ek2017@yandex.ru>
2024-01-20 11:44:11 +0300

    Homework Modul_23 Task 1:

--- Module23/01_names_2/main.py
+++ Module23/01_names_2/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+sym_sum = 0
+line_count = 0
+
+try:
+    with open('people.txt', 'r', encoding='utf-8') as user_names, open('errors.log', 'w', encoding='utf-8') as errors:
+        try:
+            for i_line in user_names:
+                line = i_line.rstrip()
+                line_count += 1
+                if len(line) < 3:
+                    raise ValueError
+                sym_sum += len(line)
+        except ValueError:
+            errors.write('Ошибка: менее трёх символов в строке {}\n'.format(line_count))
+            print('Ошибка: менее трёх символов в строке {}'.format(line_count))
+        finally:
+            print('Общее количество символов: {}'.format(sym_sum))
+except FileNotFoundError as exc:
+    print(exc, 'Файл не найден!')
