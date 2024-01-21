24d2340d4d571f471a7af3ccadf3695816c8db99
Slon4ek <Slon4ek2017@yandex.ru>
2024-01-21 10:09:17 +0300

    Homework Modul_23 Task 4: add function 'read_chat' add function 'send_message'

--- Module23/04_chat/main.py
+++ Module23/04_chat/main.py
@@ -1,1 +1,28 @@
-# TODO здесь писать код
+def read_chat():
+    try:
+        with open('chat.txt', 'r', encoding='utf-8') as chat:
+            result = chat.read()
+            print(result)
+    except FileNotFoundError:
+        print('Тут пока пусто, введите сообщение!')
+
+
+def send_message(name, text):
+    with open('chat.txt', 'a', encoding='utf-8') as chat:
+        chat.write('{:^10}\n\t\t\t>>>{:<40}\n'.format(name, text))
+
+
+user_name = input('Введите имя пользователя:')
+while True:
+    print('Выбор действия:\n\t1.Посмотреть текущий текст чата \n\t2.Отправить сообщение')
+    try:
+        answer = int(input())
+        if answer == 1:
+            read_chat()
+        if answer == 2:
+            user_input = input('Введите сообщение: ')
+            send_message(user_name, user_input)
+        if answer < 1 or answer > 2:
+            raise ValueError
+    except ValueError:
+        print('Ошибка: Введите 1 или 2')
