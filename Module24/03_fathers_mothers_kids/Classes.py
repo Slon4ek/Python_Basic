e159d57a7f7194c3b924d82ebaed2ad8c90d178a
Slon4ek <Slon4ek2017@yandex.ru>
2024-01-24 13:51:12 +0300

    Homework Modul_24 Task 3: The Kid class is described The Parent class is described

---
+++ Module24/03_fathers_mothers_kids/Classes.py
@@ -0,0 +1,73 @@
+class Kid:
+    calm_states = {0: 'Спокойствие', 1: 'Беспокойство'}
+    hunger_states = {0: 'Сытость', 1: 'Голод'}
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.calm_state = 1
+        self.hunger_state = 1
+
+    def __str__(self):
+        return '{} - {} лет'.format(self.name, self.age)
+
+    def __repr__(self):
+        return '{} : {}'.format(self.name, self.age)
+
+    def is_full(self):
+        if self.hunger_state == 0:
+            return True
+        else:
+            return False
+
+    def is_calm(self):
+        if self.calm_state == 0:
+            return True
+        else:
+            return False
+
+    def print_state(self):
+        print('{}: {} и {}'.format(
+            self.name,
+            self.hunger_states[self.hunger_state],
+            self.calm_states[self.calm_state]
+        ))
+
+
+class Parent:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.kids_list = []
+
+    def add_kid(self, kid_name, kid_age):
+        if self.age - kid_age >= 16:
+            self.kids_list.append(Kid(kid_name, kid_age))
+        else:
+            print('Возраст ребенка должен быть меньше возраста родителя хотя бы на 16 лет')
+
+    def print_info(self):
+        if len(self.kids_list) == 1:
+            print('Меня зовут {}, мне {} лет, у меня {} ребенок.'.format(
+                self.name, self.age, len(self.kids_list)))
+        else:
+            print('Меня зовут {}, мне {} лет, у меня {} детей.'.format(
+                self.name, self.age, len(self.kids_list)))
+        if self.kids_list:
+            for kid in self.kids_list:
+                print('\t', kid)
+
+    def feed(self, name):
+        for kid in self.kids_list:
+            if str(kid.name).lower() == str(name).lower():
+                if kid.hunger_state > 0:
+                    kid.hunger_state -= 1
+
+    def calm(self, name):
+        for kid in self.kids_list:
+            if str(kid.name).lower() == str(name).lower():
+                if kid.calm_state > 0:
+                    kid.calm_state -= 1
+
+
--- Module24/03_fathers_mothers_kids/main.py
+++ Module24/03_fathers_mothers_kids/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+from Classes import Parent
+
+father = Parent('Федор', 39)
+father.add_kid('Ваня', 13)
+father.add_kid('Вероника', 17)
+father.print_info()
+print(father.kids_list)
+for kid in father.kids_list:
+    if not kid.is_full() and not kid.is_calm():
+        kid.print_state()
+        father.feed(kid.name)
+        father.calm(kid.name)
+        print('Ребенок покормлен и успокоен')
+
+for kid in father.kids_list:
+    kid.print_state()
