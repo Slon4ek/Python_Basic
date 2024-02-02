c34e0800f7545373913e4fd401789f72b68e9289
Slon4ek <Slon4ek2017@yandex.ru>
2024-02-02 14:55:56 +0300

    Homework Modul_25 Task 6: The abstract class Shape is described The Circle class is described The Rectangle class is described The Triangle class is described

--- Module25/06_Abstract_class/main.py
+++ Module25/06_Abstract_class/main.py
@@ -1,5 +1,40 @@
-# TODO здесь писать код
+from abc import ABC, abstractmethod
+import math
 
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+
+class Circle(Shape, ABC):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        result = math.pi * self.radius ** 2
+        return round(result, 2)
+
+
+class Rectangle(Shape, ABC):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        result = self.length * self.width
+        return round(result, 2)
+
+
+class Triangle(Shape, ABC):
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        result = (self.base * self.height) / 2
+        return round(result, 2)
 
 
 # Примеры работы с классом:
