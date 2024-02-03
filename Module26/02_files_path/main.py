b630e6b34aecc9dd31aa766587090cb37f8b3408
Slon4ek <Slon4ek2017@yandex.ru>
2024-02-03 15:28:57 +0300

    Homework Modul_26 Task 2 complete

--- Module26/02_files_path/main.py
+++ Module26/02_files_path/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+import os
+
+
+def gen_files_path(directory: str, path=os.path.abspath(os.sep)):
+    for root, dirs, files in os.walk(path):
+        for dir_name in dirs:
+            if dir_name == directory:
+                for file in files:
+                    path_to_file = os.path.join('{}\\{}\\{}'.format(root, dir_name, file))
+                    yield path_to_file
+
+
+for files_path in gen_files_path('Python_Basic'):
+    print(files_path)
