44f776a5d19dee324489c75b9a74229c76e06c94
Slon4ek <Slon4ek2017@yandex.ru>
2024-02-03 15:00:59 +0300

    Homework Modul_26 Task 1 complete

--- Module26/01_num_squares/main.py
+++ Module26/01_num_squares/main.py
@@ -1,1 +1,26 @@
-# TODO здесь писать код
+class SquaresOfNumbers:
+    def __init__(self, n: int) -> None:
+        self.end = n
+        self.start = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self) -> int:
+        self.start += 1
+        if self.start > self.end:
+            raise StopIteration
+        return self.start ** 2
+
+
+def square(n):
+    start_num = 1
+    for num in range(n):
+        yield start_num ** 2
+        start_num += 1
+
+
+user_num = int(input('Введите число: '))
+squares = (num ** 2 for num in range(1, user_num + 1))
+for num in squares:
+    print(num)
