6de9a7ba9733538e909fc03e54cc2626163eb6bd
Slon4ek <Slon4ek2017@yandex.ru>
2024-02-06 15:53:35 +0300

    Homework Modul_27 Task 1: Described decorator haw_are_you

--- Module27/01_how_are_you/main.py
+++ Module27/01_how_are_you/main.py
@@ -1,1 +1,28 @@
-# TODO здесь писать код
+from typing import Callable, Any
+import functools
+
+
+def how_are_you(foo: Callable) -> Callable:
+    """
+    Декоратор от скуки :)
+    :param foo: Any
+    :return: wrapped function
+    """
+
+    @functools.wraps(foo)
+    def wrapped_func(*args, **kwargs):
+        print('Привет! Как дела?')
+        input('')
+        print('А у меня не очень! Скучно!!! Ладно, продолжаем работу :)')
+        result = foo(*args, **kwargs)
+        return result
+
+    return wrapped_func
+
+
+@how_are_you
+def sum_squares(n: int) -> int:
+    return sum(i ** 2 for i in range(n))
+
+
+print(sum_squares(100))
