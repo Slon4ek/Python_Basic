18c2dbcec1353ef3cb1517a9daa613ab843d8a55
Slon4ek <Slon4ek2017@yandex.ru>
2024-02-06 21:13:38 +0300

    Homework Modul_27 Task 4: The Caching class is described

--- Module27/05_caching/main.py
+++ Module27/05_caching/main.py
@@ -1,7 +1,30 @@
-# TODO здесь писать код
+from typing import Callable, Dict, Any
 
 
-def fibonacci(number):
+class Caching:
+    """
+    Класс-декоратор для кеширования результатов выполнения функции.
+    При первом запуске функции добавляет результат ее работы в словарь
+    где ключем является аргумент функции, а значение - результат выполнения.
+    При повторном вызове функции проверяется словарь и если аргумент уже присутствует
+    в словаре - берет его значение.
+    """
+
+    def __init__(self, func: Callable) -> None:
+        self.cache_dict: Dict = dict()
+        self.func = func
+
+    def __call__(self, *args) -> Any:
+        for i in args:
+            if i in self.cache_dict.keys():
+                return self.cache_dict[i]
+            else:
+                self.cache_dict[i] = self.func(i)
+                return self.cache_dict[i]
+
+
+@Caching
+def fibonacci(number: int) -> int:
     if number <= 1:
         return number
     return fibonacci(number - 1) + fibonacci(number - 2)
