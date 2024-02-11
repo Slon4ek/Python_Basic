8948abc8710ee289dcab4e07ce7792d76c87bd9d
Slon4ek <Slon4ek2017@yandex.ru>
2024-02-11 13:01:57 +0300

    Homework Modul_28 task 2: The MyMath class is described

--- Module28/02_math_module/main.py
+++ Module28/02_math_module/main.py
@@ -1,1 +1,53 @@
-# TODO здесь писать код
+import math
+
+
+class MyMath:
+    """
+    Математический модуль
+    """
+
+    @classmethod
+    def circle_len(cls, radius: int) -> float:
+        """
+        Метод вычисляет длину окружности и возвращает результат
+        :param radius: радиус окружности
+        :rtype: int
+        """
+        result = 2 * math.pi * radius
+        return result
+
+    @classmethod
+    def circle_sq(cls, radius: int) -> float:
+        """
+        Метод вычисляет площадь круга и возвращает результат
+        :param radius: радиус окружности
+        :rtype: int
+        """
+        result = math.pi * radius ** 2
+        return result
+
+    @classmethod
+    def cube_vol(cls, edge: int) -> int:
+        """
+        Метод вычисляет объем куба и возвращает результат
+        :param edge: длина ребра куба
+        :rtype: int
+        """
+        result = edge ** 3
+        return result
+
+    @classmethod
+    def sphere_sq(cls, radius: int) -> float:
+        """
+        Метод вычисляет площадь сферы и возвращает результат
+        :param radius: радиус сферы
+        :rtype: int
+        """
+        result = 4 * math.pi * radius ** 2
+        return result
+
+
+res_1 = MyMath.circle_len(radius=5)
+res_2 = MyMath.circle_sq(radius=6)
+print(res_1)
+print(res_2)
