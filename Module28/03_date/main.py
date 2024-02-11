c2031cf5ad18b5fdf38bf201234993f8cd543ae3
Slon4ek <Slon4ek2017@yandex.ru>
2024-02-11 15:02:33 +0300

    Homework Modul_28 task 3: The Date class is described

--- Module28/03_date/main.py
+++ Module28/03_date/main.py
@@ -1,1 +1,75 @@
-# TODO здесь писать код
+class DateException(Exception):
+    def __str__(self):
+        return 'Неверная дата!'
+
+
+class Date:
+    __large_months = [1, 3, 5, 6, 8, 10, 12]
+    __small_months = [4, 7, 9, 11]
+    __feb = 2
+
+    def __init__(self, day: int, month: int, year: int) -> None:
+        self._day = day
+        self._month = month
+        self._year = year
+
+    def __str__(self):
+        return 'День: {} Месяц: {} Год: {}'.format(
+            self._day,
+            self._month,
+            self._year
+        )
+
+    @property
+    def day(self) -> int:
+        return self._day
+
+    @day.setter
+    def day(self, val: int):
+        self._day = val
+
+    @property
+    def month(self) -> int:
+        return self._month
+
+    @month.setter
+    def month(self, val: int):
+        self._month = val
+
+    @property
+    def year(self) -> int:
+        return self._year
+
+    @year.setter
+    def year(self, val: int):
+        self._year = val
+
+    @classmethod
+    def from_string(cls, string: str) -> 'Date':
+        if cls.is_date_valid(string):
+            day, month, year = string.split('-')
+            return Date(day, month, year)
+        else:
+            raise DateException
+
+    @classmethod
+    def is_date_valid(cls, string: str) -> bool:
+        day, month, year = string.split('-')
+        day, month, year = int(day), int(month), int(year)
+        if 1 > month > 12:
+            return False
+        elif day < 1:
+            return False
+        elif month in cls.__large_months and day > 31:
+            return False
+        elif month in cls.__small_months and day > 30:
+            return False
+        elif month == cls.__feb and day > 28:
+            return False
+        return True
+
+
+date = Date.from_string('10-12-2077')
+print(date)
+print(Date.is_date_valid('10-12-2077'))
+print(Date.is_date_valid('40-12-2077'))
