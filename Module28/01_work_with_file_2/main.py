d650af679f226cfacc18cf25d71f9d0ab8fca0fb
Slon4ek <Slon4ek2017@yandex.ru>
2024-02-11 10:50:11 +0300

    Homework Modul_28 task 1: The File class is described

--- Module28/01_work_with_file_2/main.py
+++ Module28/01_work_with_file_2/main.py
@@ -1,1 +1,25 @@
-# TODO здесь писать код
+from typing import TextIO
+
+
+class File:
+
+    def __init__(self, filename: str, mode: str) -> None:
+        self.filename = filename
+        self.mode = mode
+        self.file = None
+
+    def __enter__(self) -> TextIO:
+        try:
+            self.file = open(self.filename, self.mode, encoding='utf8')
+        except FileNotFoundError:
+            self.file = open(self.filename, 'x', encoding='utf-8')
+            self.__enter__()
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> bool:
+        self.file.close()
+        return True
+
+
+with File('asd.txt', 'w') as file:
+    file.write('asd')
