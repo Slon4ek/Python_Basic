6bc46dfbda278ee888ff927aba989d4d9ffd3ce5
Slon4ek <Slon4ek2017@yandex.ru>
2024-02-14 11:04:58 +0300

    Homework Modul_29 Task 2: complete

--- Module29/02_callback/main.py
+++ Module29/02_callback/main.py
@@ -1,1 +1,35 @@
-# TODO здесь писать код
+from typing import Callable
+import functools
+
+
+def callback(_func: Callable = None, *, answer: str = None) -> Callable:
+    def function(func: Callable) -> Callable:
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs):
+            if answer:
+                return func(*args, **kwargs)
+            else:
+                return None
+
+        return wrapped
+
+    if _func is None:
+        return function
+    else:
+        return function(_func)
+
+
+@callback(answer='//')
+def example():
+    print('Пример функции, которая возвращает ответ сервера')
+    return 'OK'
+
+
+app = {'//': example}
+route = app.get('//')
+if route:
+    response = route()
+    print('Ответ:', response)
+else:
+    print('Такого пути нет')
+
