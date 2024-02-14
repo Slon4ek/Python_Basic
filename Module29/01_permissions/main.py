fcfc0f8517f047e446686c9e7c41339670ca0227
Slon4ek <Slon4ek2017@yandex.ru>
2024-02-14 09:51:28 +0300

    Homework Modul_29 Task 1: complete

--- Module29/01_permissions/main.py
+++ Module29/01_permissions/main.py
@@ -1,1 +1,34 @@
-# TODO здесь писать код
+from typing import Callable
+import functools
+
+
+def check_permission(user: str) -> Callable:
+    def use_func(func: Callable) -> Callable:
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            if user.title() in user_permission:
+                print(f'Access granted: coll function {func.__name__}')
+                return func()
+            else:
+                print(f'PermissionError: у пользователя недостаточно прав, чтобы выполнить функцию {func.__name__}')
+
+        return wrapper
+
+    return use_func
+
+
+user_permission = ['Admin']
+
+
+@check_permission('admin')
+def delete_site():
+    print('Удаляем сайт')
+
+
+@check_permission('user_1')
+def add_comment():
+    print('Добавляем комментарий')
+
+
+delete_site()
+add_comment()
