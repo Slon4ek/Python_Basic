101c74fefeb443b4f73027c31b9c1eafe9323194
Slon4ek <Slon4ek2017@yandex.ru>
2024-02-17 11:46:49 +0300

    Homework Modul_29 Task 5: complete

--- Module29/05_singleton/main.py
+++ Module29/05_singleton/main.py
@@ -1,1 +1,26 @@
-# TODO здесь писать код
+import functools
+
+
+def singleton(cls):
+    @functools.wraps(cls)
+    def wrapper():
+        if cls.instance is None:
+            cls.instance = cls()
+        return cls.instance
+
+    cls.instance = None
+    return wrapper
+
+
+@singleton
+class Example:
+    pass
+
+
+my_obj = Example()
+my_another_obj = Example()
+
+print(id(my_obj))
+print(id(my_another_obj))
+
+print(my_obj is my_another_obj)
