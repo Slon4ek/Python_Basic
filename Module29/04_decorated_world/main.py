7cdf2685d351e6abe362585da3556688be4bdfe7
Slon4ek <Slon4ek2017@yandex.ru>
2024-02-17 10:32:13 +0300

    Homework Modul_29 Task 4: complete

--- Module29/04_decorated_world/main.py
+++ Module29/04_decorated_world/main.py
@@ -1,1 +1,31 @@
-# TODO здесь писать код
+import functools
+from typing import Callable
+
+
+def decorator_with_args_for_any_decorator(func: Callable) -> Callable:
+    def decorator(*args, **kwargs):
+        @functools.wraps(func)
+        def wrapper(func: Callable) -> Callable:
+            print(f'Переданные арги и кварги в декоратор: {args} {kwargs}')
+            return func
+
+        return wrapper
+
+    return decorator
+
+
+@decorator_with_args_for_any_decorator
+def decorated_decorator(func: Callable):
+    @functools.wraps(func)
+    def wrapper(func: Callable) -> Callable:
+        return func()
+
+    return wrapper
+
+
+@decorated_decorator(100, 'рублей', 200, 'друзей')
+def decorated_function(text: str, num: int) -> None:
+    print("Привет", text, num)
+
+
+decorated_function("Юзер", 101)
