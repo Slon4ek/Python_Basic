1f47eaed62e0cbedc87000939c57be981353126a
Slon4ek <Slon4ek2017@yandex.ru>
2024-02-17 21:08:16 +0300

    Homework Modul_30 Task 3: complete

--- Module30/03_palindrome_the_return/main.py
+++ Module30/03_palindrome_the_return/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+from collections import Counter
+from functools import reduce
+
+
+def can_be_poly(string):
+    odd_count = sum(map(lambda x: x % 2 != 0, Counter(string).values()))
+    if odd_count > 1:
+        return False
+    else:
+        return True
+
+
+print(can_be_poly('abcba'))
+print(can_be_poly('abbbc'))
