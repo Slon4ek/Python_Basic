3c74ca040c12cfd2ae63d8183e2e8b2df94d5276
Slon4ek <Slon4ek2017@yandex.ru>
2024-02-22 11:56:23 +0300

    Homework Modul 31 Task 4: complete

--- Module31/01_lorem_ipsum/main.py
+++ Module31/01_lorem_ipsum/main.py
@@ -1,4 +1,5 @@
 import re
+
 text = """ Lorem ipsum dolor sit amet, consectetuer adipiscing elit. 
 Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, 
 nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. 
--- Module31/04_phone_numbers/main.py
+++ Module31/04_phone_numbers/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+import re
+
+phone_list = ['9999999999', '999999-999', '99999x9999']
+
+for num in phone_list:
+    if re.fullmatch(r'[89]\d{9}', num):
+        print(f'{num}: Номер в порядке')
+    else:
+        print(f'{num}: Номер не подходит')
