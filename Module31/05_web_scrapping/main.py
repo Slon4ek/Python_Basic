629b409bf3f08007f7dff48df8097716dc92fdb1
Slon4ek <Slon4ek2017@yandex.ru>
2024-02-22 12:09:24 +0300

    Homework Modul 31 Task 5: complete

--- Module31/05_web_scrapping/main.py
+++ Module31/05_web_scrapping/main.py
@@ -1,6 +1,12 @@
-# TODO здесь писать код
+import re
+import requests
 
-# В данном случае запрос request.get заменен на загрзку сайта из файла html
+my_req = requests.get('http://www.columbia.edu/~fdc/sample.html')
+if my_req.status_code == 200:
+    print(re.findall(r'<h3 .*>(.*)</h3>', my_req.text))
+
+# В данном случае запрос request.get заменен на загрузку сайта из файла html
 with open('examples.html', 'r') as f:
     text = f.read()
+    print(re.findall(r'<h3>(.*)</h3>', text))
 # По итогу вы так же получаете код сайта в виде одной строки
